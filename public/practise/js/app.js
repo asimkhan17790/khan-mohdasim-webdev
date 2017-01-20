bdf3884a11bf6cf8778e63c2d2a785b3711545f6
asimkhan17790 <asimkhan17790@gmail.com>
2017-01-19 22:12:01 -0500

    practise updated class 2

--- public/practise/js/app.js
+++ public/practise/js/app.js
@@ -8,8 +8,8 @@ angular.module("TodoApp",[])
 function TodoController ($scope) {
 console.log("Hello from controller");
     $scope.hello = 'hello world';
-    $scope.todos=[{'title':'1', 'note':'Note 1'},
-        {'title':'2', 'note':'Note 2'},
-        {'title':'3', 'note':'Note 3'},
-        {'title':'4', 'note':'Note 4'}];
+    $scope.todos=[{'title':'Title 1', 'note':'Note 1'},
+        {'title':'Title 2', 'note':'Note 2'},
+        {'title':'Title 3', 'note':'Note 3'},
+        {'title':'Title 4', 'note':'Note 4'}];
 }
