dfcc5fc6b71191c43c4482826a891305e8f76f05
asimkhan17790 <asimkhan17790@gmail.com>
2017-01-20 02:24:31 -0500

    asim

--- public/practise/js/app.js
+++ public/practise/js/app.js
@@ -5,11 +5,50 @@
 angular.module("TodoApp",[])
     .controller("TodoController", TodoController);
 
-function TodoController ($scope) {
+function TodoController ($scope,$http) {
 console.log("Hello from controller");
     $scope.hello = 'hello world';
-    $scope.todos=[{'title':'Title 1', 'note':'Note 1'},
+ /*   $scope.todos=[{'title':'Title 1', 'note':'Note 1'},
         {'title':'Title 2', 'note':'Note 2'},
         {'title':'Title 3', 'note':'Note 3'},
-        {'title':'Title 4', 'note':'Note 4'}];
+        {'title':'Title 4', 'note':'Note 4'}];*/
+
+    $scope.todos=[];
+   $http.get('/practise/todo').success(function (res) {
+        console.log(res);
+       $scope.todos=res;
+    });
+
+    $scope.createTodo = createTodo;
+    $scope.deleteTodo = deleteTodo;
+    $scope.selectTodo = selectTodo;
+    $scope.updateTodo = updateTodo;
+    $scope.selectedIndex = -1;
+
+    function createTodo(todoData) {
+   var temp =  {
+      title:todoData.title,
+      note:todoData.note
+  };
+    $scope.todos.push(temp);
+
+    }
+
+    function deleteTodo(todo) {
+      var index = $scope.todos.indexOf(todo);
+      $scope.todos.splice(index,1);
+
+    }
+    function selectTodo(todo) {
+        $scope.selectedIndex= $scope.todos.indexOf(todo);
+        $scope.todoData ={};
+        $scope.todoData.title = todo.title;
+        $scope.todoData.note = todo.note;
+
+    }
+    function updateTodo (todoData) {
+    $scope.todos[$scope.selectedIndex].title=todoData.title;
+    $scope.todos[$scope.selectedIndex].note=todoData.note;
+    $scope.todoData ={};
+    }
 }
---
+++ todo/app.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Asim on 20-01-2017.
+ */
+module.exports = function(app){
+
+
+
+    app.get('/hello/:name', function (req,res) {
+        res.send('hello world' + req.params.name + 'Age:'+ req.query.age);
+    });
+
+
+    app.get('/practise/todo',function (req,res) {
+
+
+        var todos=[{'title':'Title 1 server', 'note':'Note 1'},
+            {'title':'Title 2 server', 'note':'Note 2'},
+            {'title':'Title 3 server', 'note':'Note 3'},
+            {'title':'Title 4 server', 'note':'Note 4'}];
+
+
+        res.send(todos);
+    });
+};
