21378bf71ee2526210e2c6c664c135064e296301
asimkhan17790 <asimkhan17790@gmail.com>
2017-01-25 04:03:42 -0500

    website list incomplete

---
+++ public/assignment/websites/js/weblist-app.js
@@ -0,0 +1,14 @@
+angular
+    .module("WebsiteListApp", [])
+    .controller("WebsiteListController", WebsiteListController)
+
+function WebsiteListController($scope, $http) {
+    $scope.websiteList = [
+        {name:'Address Book App', description:'Address Book App Description'},
+        {name:'Blogger', description:'Blogger App Description'},
+        {name:'Blogging App', description:'Blogging App Description'},
+        {name:'Script Testing App', description:'Script Testing App Description'}
+    ];
+
+
+}
