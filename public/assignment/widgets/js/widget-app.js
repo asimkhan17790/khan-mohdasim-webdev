9aef6e5568235c53a1f53136f09cd4ffa52d6161
asimkhan17790 <asimkhan17790@gmail.com>
2017-01-31 17:46:16 -0500

    widgets pages updated

--- public/assignment/widgets/js/widget-app.js
+++ public/assignment/widgets/js/widget-app.js
@@ -3,6 +3,7 @@ angular
     .controller("WidgetChooserController", WidgetChooserController);
 
 function WidgetChooserController($scope, $http) {
+    $scope.data=40;
     $scope.widgetList = [
         {name:'Header', route:'widget-heading.html'},
         {name:'Image', route:'widget-image.html'},
