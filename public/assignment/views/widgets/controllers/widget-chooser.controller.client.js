e854ef0f0fd7253e6ae8bdbc43253ae61f904a70
asimkhan17790 <asimkhan17790@gmail.com>
2017-02-15 23:12:16 -0500

    some more changes

--- public/assignment/views/widgets/controllers/widget-chooser.controller.client.js
+++ public/assignment/views/widgets/controllers/widget-chooser.controller.client.js
@@ -4,19 +4,18 @@
         .module("WebAppMaker")
         .controller("WidgetChooserController", WidgetChooserController);
 
-    function WidgetChooserController($routeParams, WidgetService,StaticDataService) {
+    function WidgetChooserController($routeParams, StaticDataService) {
 
         var vm = this;
         vm.userId = $routeParams['uid'];
         vm.websiteId = $routeParams['wid'];
         vm.pageId = $routeParams['pid'];
 
-        vm.availableWidgets = StaticDataService.widgetOptions;
-
-        function getWidgetTemplateUrl(widgetType) {
-            var url = 'views/widget/templates/widget-'+widgetType+'.view.client.html';
-            return url;
+        function init () {
+            vm.availableWidgets = StaticDataService.widgetOptions;
         }
+        init();
+
     }
 
 
