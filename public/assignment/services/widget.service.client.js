7d3ab62935efb291bd871234e849ba2ea676b639
asimkhan17790 <asimkhan17790@gmail.com>
2017-02-15 22:34:08 -0500

    updated yoututbe url

--- public/assignment/services/widget.service.client.js
+++ public/assignment/services/widget.service.client.js
@@ -13,7 +13,7 @@
             { "_id": "126", "widgetType": "HTML", "pageId": "321", "text": '<p>Anker’s kevlar-reinforced PowerLine cables are <a href="http://gear.lifehacker.com/your-favorite-lightning-cables-anker-powerline-and-pow-1782036601" target="_blank" rel="noopener">far and away our readers’ top choice for charging their gadgets</a>, and you can save on several models today, including some from the nylon-wrapped PowerLine+ collection. I use these cables every single day, and I’ve never had one fray or stop working. Just be sure to note the promo codes below.<br></p>'},
             { "_id": "127", "widgetType": "HEADER", "pageId": "321", "size": 4, "text": "Lorem ipsum"},
             { "_id": "128", "widgetType": "YOUTUBE", "pageId": "321", "width": "100%",
-                "url": "https://youtu.be/AM2Ivdi9c4E" },
+                "url": "https://youtu.be/3AtDnEC4zak" },
             { "_id": "129", "widgetType": "HTML", "pageId": "321", "text": "<p>Lorem ipsum</p>"}
 
 
@@ -24,7 +24,7 @@
             { "_id": "133", "widgetType": "HTML", "pageId": "547", "text": '<p>Anker’s kevlar-reinforced PowerLine cables are <a href="http://gear.lifehacker.com/your-favorite-lightning-cables-anker-powerline-and-pow-1782036601" target="_blank" rel="noopener">far and away our readers’ top choice for charging their gadgets</a>, and you can save on several models today, including some from the nylon-wrapped PowerLine+ collection. I use these cables every single day, and I’ve never had one fray or stop working. Just be sure to note the promo codes below.<br></p>'},
             { "_id": "134", "widgetType": "HEADER", "pageId": "547", "size": 4, "text": "Lorem ipsum"},
             { "_id": "135", "widgetType": "YOUTUBE", "pageId": "547", "width": "100%",
-                "url": "https://youtu.be/AM2Ivdi9c4E" },
+                "url": "https://youtu.be/3AtDnEC4zak" },
             { "_id": "136", "widgetType": "HTML", "pageId": "547", "text": "<p>Lorem ipsum</p>"},
 
             { "_id": "137", "widgetType": "HEADER", "pageId": "546", "size": 2, "text": "GIZMODO"},
@@ -34,7 +34,7 @@
             { "_id": "140", "widgetType": "HTML", "pageId": "546", "text": '<p>Anker’s kevlar-reinforced PowerLine cables are <a href="http://gear.lifehacker.com/your-favorite-lightning-cables-anker-powerline-and-pow-1782036601" target="_blank" rel="noopener">far and away our readers’ top choice for charging their gadgets</a>, and you can save on several models today, including some from the nylon-wrapped PowerLine+ collection. I use these cables every single day, and I’ve never had one fray or stop working. Just be sure to note the promo codes below.<br></p>'},
             { "_id": "141", "widgetType": "HEADER", "pageId": "546", "size": 4, "text": "Lorem ipsum"},
             { "_id": "142", "widgetType": "YOUTUBE", "pageId": "546", "width": "100%",
-                "url": "https://youtu.be/AM2Ivdi9c4E" },
+                "url": "https://youtu.be/3AtDnEC4zak" },
             { "_id": "143", "widgetType": "HTML", "pageId": "546", "text": "<p>Lorem ipsum</p>"},
 
             { "_id": "144", "widgetType": "HEADER", "pageId": "545", "size": 2, "text": "GIZMODO"},
@@ -44,7 +44,7 @@
             { "_id": "147", "widgetType": "HTML", "pageId": "545", "text": '<p>Anker’s kevlar-reinforced PowerLine cables are <a href="http://gear.lifehacker.com/your-favorite-lightning-cables-anker-powerline-and-pow-1782036601" target="_blank" rel="noopener">far and away our readers’ top choice for charging their gadgets</a>, and you can save on several models today, including some from the nylon-wrapped PowerLine+ collection. I use these cables every single day, and I’ve never had one fray or stop working. Just be sure to note the promo codes below.<br></p>'},
             { "_id": "148", "widgetType": "HEADER", "pageId": "545", "size": 4, "text": "Lorem ipsum"},
             { "_id": "149", "widgetType": "YOUTUBE", "pageId": "545", "width": "100%",
-                "url": "https://youtu.be/AM2Ivdi9c4E" },
+                "url": "https://youtu.be/3AtDnEC4zak" },
             { "_id": "150", "widgetType": "HTML", "pageId": "545", "text": "<p>Lorem ipsum</p>"},
 
             { "_id": "151", "widgetType": "HEADER", "pageId": "544", "size": 2, "text": "GIZMODO"},
@@ -54,7 +54,7 @@
             { "_id": "154", "widgetType": "HTML", "pageId": "544", "text": '<p>Anker’s kevlar-reinforced PowerLine cables are <a href="http://gear.lifehacker.com/your-favorite-lightning-cables-anker-powerline-and-pow-1782036601" target="_blank" rel="noopener">far and away our readers’ top choice for charging their gadgets</a>, and you can save on several models today, including some from the nylon-wrapped PowerLine+ collection. I use these cables every single day, and I’ve never had one fray or stop working. Just be sure to note the promo codes below.<br></p>'},
             { "_id": "155", "widgetType": "HEADER", "pageId": "544", "size": 4, "text": "Lorem ipsum"},
             { "_id": "156", "widgetType": "YOUTUBE", "pageId": "544", "width": "100%",
-                "url": "https://youtu.be/AM2Ivdi9c4E" },
+                "url": "https://youtu.be/3AtDnEC4zak" },
             { "_id": "157", "widgetType": "HTML", "pageId": "544", "text": "<p>Lorem ipsum</p>"},
 
             { "_id": "158", "widgetType": "HEADER", "pageId": "543", "size": 2, "text": "GIZMODO"},
@@ -64,7 +64,7 @@
             { "_id": "161", "widgetType": "HTML", "pageId": "543", "text": '<p>Anker’s kevlar-reinforced PowerLine cables are <a href="http://gear.lifehacker.com/your-favorite-lightning-cables-anker-powerline-and-pow-1782036601" target="_blank" rel="noopener">far and away our readers’ top choice for charging their gadgets</a>, and you can save on several models today, including some from the nylon-wrapped PowerLine+ collection. I use these cables every single day, and I’ve never had one fray or stop working. Just be sure to note the promo codes below.<br></p>'},
             { "_id": "162", "widgetType": "HEADER", "pageId": "543", "size": 4, "text": "Lorem ipsum"},
             { "_id": "163", "widgetType": "YOUTUBE", "pageId": "543", "width": "100%",
-                "url": "https://youtu.be/AM2Ivdi9c4E" },
+                "url": "https://youtu.be/3AtDnEC4zak" },
             { "_id": "164", "widgetType": "HTML", "pageId": "543", "text": "<p>Lorem ipsum</p>"},
             { "_id": "165", "widgetType": "HEADER", "pageId": "432", "size": 2, "text": "GIZMODO"},
             { "_id": "166", "widgetType": "HEADER", "pageId": "432", "size": 4, "text": "Lorem ipsum"},
@@ -73,7 +73,7 @@
             { "_id": "168", "widgetType": "HTML", "pageId": "432", "text": '<p>Anker’s kevlar-reinforced PowerLine cables are <a href="http://gear.lifehacker.com/your-favorite-lightning-cables-anker-powerline-and-pow-1782036601" target="_blank" rel="noopener">far and away our readers’ top choice for charging their gadgets</a>, and you can save on several models today, including some from the nylon-wrapped PowerLine+ collection. I use these cables every single day, and I’ve never had one fray or stop working. Just be sure to note the promo codes below.<br></p>'},
             { "_id": "169", "widgetType": "HEADER", "pageId": "432", "size": 4, "text": "Lorem ipsum"},
             { "_id": "170", "widgetType": "YOUTUBE", "pageId": "432", "width": "100%",
-                "url": "https://youtu.be/AM2Ivdi9c4E" },
+                "url": "https://youtu.be/3AtDnEC4zak" },
             { "_id": "171", "widgetType": "HTML", "pageId": "432", "text": "<p>Lorem ipsum</p>"}
         ];
 
