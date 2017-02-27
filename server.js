a786bec11b0b608ec13b172c5b3a83183264dbb2
asimkhan17790 <asimkhan17790@gmail.com>
2017-02-27 07:27:31 -0500

    updated server

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+var passport      = require('passport');
+var cookieParser  = require('cookie-parser');
+var session       = require('express-session');
+
 // configure a public directory to host static content
 app.use(express.static(__dirname + '/public'));
 
